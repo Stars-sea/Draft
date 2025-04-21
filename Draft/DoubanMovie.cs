c7675a4c78fa33ad461fdec7c41e000c261737ef
Stars sea <Stars_sea@outlook.com>
2025-04-21 11:57:50 +0800

    [work] Douban Movie Top 250

---
+++ Draft/DoubanMovie.cs
@@ -0,0 +1,28 @@
+﻿using HtmlAgilityPack;
+
+namespace Draft;
+
+public record DoubanMovie(
+    string Title,
+    string Description,
+    string Rating,
+    string Quote,
+    string Url
+)
+{
+    public static DoubanMovie ParseFromHtml(HtmlNode node)
+    {
+        string title = node.SelectSingleNode(".//span[@class='title']")!.InnerText;
+        string description = node.SelectSingleNode(".//div[@class='bd']/p")!.InnerText;
+        string rating = node.SelectSingleNode(".//span[@class='rating_num']")!.InnerText;
+        string quote = node.SelectSingleNode(".//p[@class='quote']/span")?.InnerText ?? string.Empty;
+        string url = node.SelectSingleNode(".//div[@class='hd']/a")!.GetAttributeValue("href", "");
+
+        description = string.Join('\n',
+            description.Split('\n')
+                       .Where(s => !string.IsNullOrWhiteSpace(s))
+                       .Select(s => s.Trim()));
+        
+        return new DoubanMovie(title, description, rating, quote, url);
+    }
+}
--- Draft/Program.cs
+++ Draft/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using Draft;
+using HtmlAgilityPack;
 
-Console.WriteLine("Hello, World!");
+const string url = "https://movie.douban.com/top250";
+
+HtmlWeb web = new()
+{
+    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
+};
+
+List<DoubanMovie> movies = [];
+for (var i = 0; i < 10; i++)
+{
+    string currentUrl = i == 0 ? url : $"{url}?start={i * 25}&filter=";
+    movies.AddRange(await FetchDoubanPage(currentUrl));
+}
+
+if (File.Exists("movies.json"))
+    File.Delete("movies.json");
+
+await using FileStream file = File.OpenWrite("movies.json");
+await JsonSerializer.SerializeAsync(file, movies, new JsonSerializerOptions()
+{
+    WriteIndented = true,
+    Encoder       = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+});
+
+return ;
+
+async Task<IEnumerable<DoubanMovie>> FetchDoubanPage(string currentUrl)
+{
+    HtmlDocument doc = await web.LoadFromWebAsync(currentUrl);
+    HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//div[@class='item']") ?? throw new InvalidOperationException();
+
+    return nodes.Select(DoubanMovie.ParseFromHtml);
+}
